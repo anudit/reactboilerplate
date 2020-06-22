5030aa62ce36cab01779570570cfa5cda7f116e4
Anudit Nagar <nagaranudit@gmail.com>
2020-06-22 09:14:13 +0530

    Contextual Login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react'
 
 import './App.css';
 
@@ -8,15 +8,78 @@ import About from './pages/About';
 
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
+import Web3 from "web3";
+import Web3Modal from "web3modal";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import Torus from "@toruslabs/torus-embed";
+
+import Web3context from './contexts/Web3context.js'
+
 function App() {
+
+  const [getWeb3, setWeb3] = useState(undefined)
+
+  const providerOptions = {
+    walletconnect: {
+      package: WalletConnectProvider,
+      options: {
+        rpc: {
+          16110: "https://betav2.matic.network"
+        }
+      }
+    },
+    torus: {
+      package: Torus,
+      options: {
+        networkParams: {
+          host: "https://betav2.matic.network",
+          chainId: 16110,
+          networkId: 16110
+        },
+        config: {
+          buildEnv: "development"
+        }
+      }
+    }
+  };
+
+  const web3Modal = new Web3Modal({
+    cacheProvider: false,
+    theme: "dark",
+    providerOptions
+  });
+
+  async function loginWeb3(){
+    let provider = await web3Modal.connect()
+    let w3 = new Web3(provider)
+    setWeb3(w3)
+    console.log(w3)
+  }
+
+  async function logoutWeb3(){
+    web3Modal.clearCachedProvider();
+    setWeb3(undefined)
+  }
+
+  async function isLogin(){
+    if (getWeb3 === undefined){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+
   return (
     <Router>
       <div className="App">
         <Nav/>
-        <Switch>
-          <Route path="/" exact component={Home}/>
-          <Route path="/about" component={About}/>
-        </Switch>
+        <Web3context.Provider value={{getWeb3, loginWeb3, logoutWeb3, isLogin}}>
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/about" component={About} />
+          </Switch>
+        </Web3context.Provider>
       </div>
     </Router>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,33 @@
-import React, { useState, useContext, useEffect } from 'react'
-import {TODO_LIST_ADDRESS, TODO_LIST_ABI} from '../contractData'
-// import getWeb3Setup from '../web3Helper'
+import React, { useContext, useEffect } from 'react'
+import Web3context from '../contexts/Web3context.js'
+
 import '../App.css';
-
-import Web3 from "web3";
-import Web3Modal from "web3modal";
-import WalletConnectProvider from "@walletconnect/web3-provider";
-import Torus from "@toruslabs/torus-embed";
-
-
 export default function Home () {
 
-  const [getWeb3, setWeb3] = useState({})
-  const providerOptions = {
-    walletconnect: {
-      package: WalletConnectProvider,
-      options: {
-        rpc: {
-          16110: "https://betav2.matic.network"
-        }
-      }
-    },
-    torus: {
-      package: Torus,
-      options: {
-        networkParams: {
-          host: "https://betav2.matic.network",
-          chainId: 16110,
-          networkId: 16110
-        },
-        config: {
-          buildEnv: "development"
-        }
-      }
-    }
-  };
+  const web3context = useContext(Web3context)
+  let {getWeb3, logoutWeb3, loginWeb3} = web3context;
 
-  const web3Modal = new Web3Modal({
-    cacheProvider: false,
-    providerOptions
-  });
+  useEffect(()=>{
 
-  useEffect(() => {
-
-  }, []);
-
-  async function setupLogin(){
-    let provider = await web3Modal.connect()
-    let w3 = new Web3(provider)
-    setWeb3(w3)
-    console.log(web3Modal)
-    console.log(w3)
-  }
+  }, [])
 
   return (
+
     <div>
-      <p>Acc: {getWeb3.selectedAddress}</p>
-      <button onClick={setupLogin}>
-        {getWeb3.currentProvider != null ? 'Logged In' : 'Login'}
-      </button>
+
+      {getWeb3 !== undefined
+        ?
+          <div>
+            <p>Acc: {getWeb3.currentProvider.selectedAddress}</p>
+            <button onClick={logoutWeb3}>
+              Log Out
+            </button>
+          </div>
+        : <button onClick={loginWeb3}>
+            Log In
+          </button>
+      }
+
     </div>
     )
 }
